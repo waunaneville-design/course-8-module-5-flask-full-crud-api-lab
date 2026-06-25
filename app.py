2ec2cba1745e3d38024b15f37b2846052c8dabe4
Lascelles <waunaneville@gmail.com>
2026-06-25 14:36:07 +0300

    Add Event class with id and title attributes

--- app.py
+++ app.py
@@ -2,3 +2,8 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+# Simulated data
+class Event:
+    def __init__(self, id, title):
+        self.id = id
+        self.title = title
